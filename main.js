e74d7e3da69f4f2985604ce4cd33c07d2abbf798
beatryzcavalcanti32-crypto <beatryzcavalcanti32@gmail.com>
2026-09-21 20:21:46 -0300

    consertei o index

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
+
 var a = 5
 var b = 7
-var c = a + b
-console.log(c)
+var c = a+b
+console.log(c) 
+
+
+/*MODELO 2: FUNÇÕES
+var a = 3
+var b = 4
+function somar (x,y){
+    var c = x+y
+    return c
+}
+
+console.log(somar(a,b))
+FIM MODELO 2*/
+
+/*MODELO 3: TIPOS DE VARIÁVEIS
+var a = 0
+let b = 0
+const d = 0
+
+
+function somar (x,y){
+    let c = x+y
+    return c
+}
+
+FIM MODELO 3*/
